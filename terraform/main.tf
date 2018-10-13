2a2fff2d454fcc08424e732540f81f29c3de8cf5
horia-cloud <starashoria@yahoo.com>
2018-10-13 01:00:01 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
 
+resource "aws_instance" "my-test-instance" {
+  ami           = "${data.aws_ami.centos7.id}"
+  instance_type = "${var.ec2_instance_type}"
+  key_name      = "${var.ec2_key_name}"
+  user_data     = "${file("userdata.sh")}"
+
+  tags {
+    Name = "RV"
+  }
+
+  security_groups = [
+    "${aws_security_group.allow_ssh1.name}",
+  ]
+
+  lifecycle {
+    ignore_changes = ["user_data"]
+  }
+}
