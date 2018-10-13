5ef44d410b7ae414da48f4b9f4500dfd6f95aa1d
horia-cloud <starashoria@yahoo.com>
2018-10-13 01:07:32 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "ec2_instance_type" {
+  default = "t2.small"
+}
+
+variable "tv_key_name" {
+  default = "test_rv"
+}
