391bea27d179965da04cd69c9c50ec49f817cd5e
horia-cloud <starashoria@yahoo.com>
2018-10-13 01:01:33 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "my-test-instance" {
-  ami           = "${data.aws_ami.centos7.id}"
+  ami           = "${data.aws_ami.amazonami.id}"
   instance_type = "${var.ec2_instance_type}"
   key_name      = "${var.ec2_key_name}"
   user_data     = "${file("userdata.sh")}"
