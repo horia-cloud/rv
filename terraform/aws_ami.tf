df671fa19cefe36a8e4b330d50cba34cb73179e2
horia-cloud <starashoria@yahoo.com>
2018-10-13 01:01:16 +0100

    Create aws_ami.tf

---
+++ terraform/aws_ami.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "amazonami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+}
