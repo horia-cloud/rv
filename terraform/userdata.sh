a12cc901ede5e43ab3623c0d6f4e7fd32d3db252
horiacloud <starashoria@yahoo.com>
2018-10-16 00:18:47 +0100

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -6,6 +6,5 @@ yum install -y docker git
 
 service docker start
 
-docker pull horiacloud/rv-docker
-
-docker run -p 80:80 -it --rm --name rv horiacloud/rv-docker
+docker pull horiacloud/rv-docker  
+docker run -p 80:80 -it --rm --name rv horiacloud/rv-docker  
